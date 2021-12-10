d9a73e76de09fdfc6748a6e37cf84eede8cdd7a4
[Erik Bahena] <[erikjbahena@gmail.com]>
2021-12-10 14:25:24 -0800

    wip

--- api/project/model.js
+++ api/project/model.js
@@ -5,7 +5,8 @@ const find = async () => {
   const projects = await db("projects");
 
   projects.forEach((project) => {
-    if (project.project_completed === 0) project.project_completed = false;
+    if (project.project_completed === 0 || project.project_completed === null)
+      project.project_completed = false;
     else project.project_completed = true;
   });
 
@@ -13,7 +14,7 @@ const find = async () => {
 };
 
 const findById = async (project_id) => {
-  const project = await db("projects").where("project_id", project_id).first();
+  const project = await db("projects").where({ project_id }).first();
 
   if (project.project_completed === 0) project.project_completed = false;
   else project.project_completed = true;
--- api/task/model.js
+++ api/task/model.js
@@ -1,16 +1,31 @@
-// build your `Task` model here
 const db = require("../../data/dbConfig");
 
 const find = async () => {
-  return await db("tasks");
+  const tasks = await db("tasks as t")
+    .join("projects as p", "t.project_id", "p.project_id")
+    .select("t.*", "p.project_description", "p.project_name");
+
+  tasks.forEach((task) => {
+    if (task.task_completed === 0 || task.task_completed === null)
+      task.task_completed = false;
+    else task.task_completed = true;
+  });
+
+  return tasks;
 };
 
 const findById = async (task_id) => {
-  return await db("tasks").where({ task_id });
+  const task = await db("tasks").where({ task_id }).first();
+
+  if (task.task_completed === 0 || task.task_completed === null)
+    task.task_completed = false;
+  else task.task_completed = true;
+
+  return task;
 };
 
 const create = async (newTask) => {
-  const [newTaskId] = db("tasks").insert(newTask);
+  const [newTaskId] = await db("tasks").insert(newTask);
 
   return findById(newTaskId);
 };
