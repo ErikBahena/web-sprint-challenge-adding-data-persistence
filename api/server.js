ef3450401a38ba32360ca2c2ad128142256a676e
[Erik Bahena] <[erikjbahena@gmail.com]>
2021-12-10 10:13:22 -0800

    server instantiated and using error handling throughout all endpoints

--- api/server.js
+++ api/server.js
@@ -1,1 +1,12 @@
-// build your server here and require it from index.js
+const express = require("express");
+
+const server = express();
+server.use(express.json());
+
+server.use((err, next, req, res) => {
+  res
+    .status(err.status || 500)
+    .json({ message: err.message, stack: err.stack });
+});
+
+module.exports = server;
